f9681684ba11d2f2ba02b9e83b1c75f9ece9c19e
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2023-09-03 19:18:43 -0300

    Passo 3 - Tutorial Angular parte 2

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrls: ['./housing-location.component.css']
+})
+export class HousingLocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
