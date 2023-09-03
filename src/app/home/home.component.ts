416020d1477c001bc7dc7927ae04712f0661e307
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2023-09-03 19:17:29 -0300

    Passo 3 - Tutorial angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  template: `
-      <section>
-        <form>
-          <input type="text" placeholder="Filtrar">
-          <button class="primary" type="button">Pesquisar</button>
-        </form>
-      </section>`,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
