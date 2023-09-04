a8decdb5afa5daba9a0fbdd9eb407f730d032265
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2023-09-03 23:30:43 -0300

    Passo 4,5,6 e 7 - Tutorial Angular

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
+  templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent implements OnInit {
+
+  @Input() housingLocation!: HousingLocation;
 
   constructor() { }
 
